271d32939b7852676d9dd34117017fbdabfb8623
darlaevans2000 <darlaevans2000@gmail.com>
2021-07-12 15:38:16 -0600

    Add user class and method

---
+++ src/User.js
@@ -0,0 +1,18 @@
+class User {
+  constructor(user) {
+    this.id = user.id;
+    this.name = user.name;
+    this.address = user.address;
+    this.email = user.email;
+    this.stride = user.strideLength;
+    this.dailyStepGoal = user.dailyStepGoal;
+    this.friends = user.friends;
+  }
+
+  returnFirstName() {
+    const firstName = this.name.split(' ');
+    return firstName;
+  }
+}
+
+export default User;
