3bf9f53f2f314e9ed164954a04bfeeb754818e64
William McGuire <wmcguire18@gmail.com>
2021-07-13 04:07:20 -0400

    Added scripts functionality

--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,33 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS file
+import UserRepository from './UserRepository';
+import User from './User';
+import Activity from './Activity';
+import Hydration from './Hydration';
+import Sleep from './Sleep';
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
-
-// An example of how you tell webpack to use a JS file
-
-import userData from './data/users';
-import UserRepository from './UserRepository';
 
 const homeBtn = document.getElementById('headerDate');
 const userInfoBtn = document.getElementById('headerMessage');
 const homePage = document.getElementById('homePage');
 const userInfoPage = document.getElementById('userInfo');
 
+function generateRandomNum(list){
+  return Math.round(Math.random() * list.length);
+}
 
-//hide and show DOM functions
+const newUserID = Math.floor(Math.random() * (50 - 1 + 1)) + 1;
+
+fetchData('users/userData')
+  .then(userData => {
+    const userRepo = new UserRepo(userData.userData);
+    const user = new User(userRepo.returnUserData(uniqueUserID));
+}
+
+//displayUserName()
+
+//compareStepGoal()
+
 function hide(elements) {
   elements.forEach(element => element.classList.add('hidden'));
 }
@@ -29,3 +36,11 @@ function show(elements) {
   elements.forEach(element => element.classList.remove('hidden'))
 }
 
+
+
+
+
+
+
+
+// For a specific user, display how their step goal compares to the average step goal amongst all users (this display should not be hard-coded)
