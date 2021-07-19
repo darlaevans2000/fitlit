9fe118d6b3873370440c6265248d58d395859918
darlaevans2000 <darlaevans2000@gmail.com>
2021-07-19 10:54:28 -0600

    Add activity test data

---
+++ src/data/activityTestData.js
@@ -0,0 +1,60 @@
+const activityTestData = [
+  {
+    "userID": 1,
+    "date": "2019/06/15",
+    "numSteps": 3517,
+    "minutesActive": 140,
+    "flightsOfStairs": 16
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numSteps": 3284,
+    "minutesActive": 78,
+    "flightsOfStairs": 10
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/17",
+    "numSteps": 10402,
+    "minutesActive": 102,
+    "flightsOfStairs": 33
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/18",
+    "numSteps": 8397,
+    "minutesActive": 54,
+    "flightsOfStairs": 23
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/19",
+    "numSteps": 3402,
+    "minutesActive": 37,
+    "flightsOfStairs": 11
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/20",
+    "numSteps": 13948,
+    "minutesActive": 320,
+    "flightsOfStairs": 30
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/21",
+    "numSteps": 12419,
+    "minutesActive": 235,
+    "flightsOfStairs": 37
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/22",
+    "numSteps": 5676,
+    "minutesActive": 153,
+    "flightsOfStairs": 13
+  }
+];
+
+module.exports = activityTestData;
